0719af03426abef735933501330accdc3995185c
Eddie Zaneski <eddiezane@gmail.com>
2015-06-05 08:41:34 -0400

    Add some oauth and views

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ var bodyParser = require('body-parser');
 var multer     = require('multer');
 var sendgrid   = require('sendgrid')(process.env.SENDGRID_APIKEY);
 var mongoose   = require('mongoose');
+var hbs        = require('express-hbs');
 var passport   = require('passport');
 var Promise    = require('bluebird');
+
+if (app.get('env') === 'development') {
+    require('dotenv').load();
+}
 
 Promise.promisifyAll(mongoose);
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pizzafy');
@@ -13,13 +18,28 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/pizzafy');
 var User = require('./models/user.js');
 
 app.set('port', process.env.PORT || 3000);
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(multer());
 
+require('./config/passport.js');
+
 app.get('/', function(req, res) {
-  res.send('hello');
+  res.render('index');
+});
+
+app.get('/login/facebook', passport.authenticate('facebook'), function(req, res) {
+
+});
+
+app.get('/login/twitter', passport.authenticate('twitter'), function(req, res) {
+  res.send('logged in');
 });
 
 app.listen(app.get('port'), function() {
--- config/passport.js
+++ config/passport.js
@@ -3,3 +3,28 @@ var LocalStrategy    = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy  = require('passport-twitter').Strategy;
 
+// passport.use(new FacebookStrategy({
+  // clientID: FACEBOOK_APP_ID,
+  // clientSecret: FACEBOOK_APP_SECRET,
+  // callbackURL: "http://localhost:3000/auth/facebook/callback",
+  // enableProof: false
+// }, function(accessToken, refreshToken, profile, done) {
+  // // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+  // // return done(err, user);
+  // // });
+  // console.log(profile);
+  // done(null, 'test');
+// }));
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+}, function(token, tokenSecret, profile, done) {
+  // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+    // return done(err, user);
+  // });
+  console.log('twitter token', token);
+  console.log(profile);
+  done(null, 'test');
+}));
